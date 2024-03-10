7b3dd516058849d5f9a6ff241bb902bf93f41ae7
Zaid Ghazal <zaid.ghazal20@gmail.com>
2024-03-11 01:48:23 +0300

    Add winner and terminal functions implementation and their tests

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,6 @@
 import unittest
 
-from tictactoe import actions, player, result
+from tictactoe import actions, player, result, terminal, winner
 
 X = "X"
 O = "O"
@@ -80,3 +80,55 @@ class testTicTacToe(unittest.TestCase):
                 self.assertEqual(result(board_4, (1,1)), [[EMPTY, EMPTY, EMPTY],
                         [EMPTY, X, EMPTY],
                         [EMPTY, EMPTY, EMPTY]])
+
+        def test_terminal(self):
+                """This function tests the terminal function in tictactoe.py """
+                board_1 = [[EMPTY, X, O],
+                        [EMPTY, X, O],
+                        [EMPTY, EMPTY, EMPTY]]
+                
+                board_2 = [[EMPTY, X, O],
+                        [X, X, O],
+                        [EMPTY, EMPTY, EMPTY]]
+                
+                board_3 = [ [O, X, O],
+                        [X, X, O],
+                        [X, O, X]]
+                
+                board_4 = [[EMPTY, EMPTY, EMPTY],
+                        [EMPTY, EMPTY, EMPTY],
+                        [EMPTY, EMPTY, EMPTY]]
+                
+                self.assertEqual(terminal(board_1), False)
+                self.assertEqual(terminal(board_2), False)
+                self.assertEqual(terminal(board_3), True)
+                self.assertEqual(terminal(board_4), False)
+        
+        def test_winner(self):
+                """This function tests the winner function in tictactoe.py """
+                board_1 = [[EMPTY, X, O],
+                        [EMPTY, X, O],
+                        [EMPTY, EMPTY, EMPTY]]
+                
+                board_2 = [[EMPTY, X, O],
+                           [EMPTY, X, O],
+                           [EMPTY, X, EMPTY]]
+                
+                board_3 = [     [O, O, X],
+                                [O, X, EMPTY],
+                                [X, EMPTY, X]]
+                
+                board_4 = [[EMPTY, EMPTY, EMPTY],
+                        [EMPTY, EMPTY, EMPTY],
+                        [EMPTY, EMPTY, EMPTY]]
+                
+                board_5 = [     [O, O, O],
+                                [X, X, O],
+                                [X, X, EMPTY]]
+                
+                self.assertEqual(winner(board_1), None)
+                self.assertEqual(winner(board_2), X)
+                self.assertEqual(winner(board_3), X)
+                self.assertEqual(winner(board_4), None)
+                self.assertEqual(winner(board_5), O)
+
--- tictactoe.py
+++ tictactoe.py
@@ -66,14 +66,33 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != None:
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != None:
+            return board[0][i]
+        
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != None:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != None:
+        return board[0][2]
+    
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) != None:
+        return True
+    
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                return False
+    
+    return True
 
 
 def utility(board):
